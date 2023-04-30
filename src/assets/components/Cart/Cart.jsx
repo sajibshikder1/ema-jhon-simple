dbf5088a6aa6ec0485121de582bd3090321fce54
sajibshikder1 <sajibshikder8096@gmail.com>
2023-04-30 21:09:51 +0600

    retrive product from stored cart and setit to cart state

--- src/assets/components/Cart/Cart.jsx
+++ src/assets/components/Cart/Cart.jsx
@@ -6,17 +6,19 @@ const Cart = ({cart}) => {
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for(const product of cart){
      totalPrice = totalPrice + product.price;
      totalShipping = totalShipping + product.shipping;
   }
   const tax = totalPrice *7/100;
   const grandTotal = totalPrice + totalShipping + tax;
+  // quantity = quantity + product.quantity;
   
   return (
     <div className="cart">
       <h4>Order summary</h4>
-      <p>Selected items: {cart.length}</p>
+      <p>Selected items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charge: ${totalShipping} </p>
       <p>Tax: ${tax.toFixed(2)}</p>
