0658f772999edd6e9fe23e0873aaa16aba0d53a7
sajibshikder1 <sajibshikder8096@gmail.com>
2023-05-01 11:12:32 +0600

    show data from cart

--- src/assets/components/Cart/Cart.jsx
+++ src/assets/components/Cart/Cart.jsx
@@ -8,12 +8,21 @@ const Cart = ({cart}) => {
   let totalShipping = 0;
   let quantity = 0;
   for(const product of cart){
-     totalPrice = totalPrice + product.price;
+
+    // if(product.quantity === 0){
+    //   product.quantity = 1;
+    // }
+
+    // shortcut
+    // product.quantity = product.quantity || 1;
+
+     totalPrice = totalPrice + product.price * product.quantity;
      totalShipping = totalShipping + product.shipping;
+     quantity = quantity + product.quantity;
   }
   const tax = totalPrice *7/100;
   const grandTotal = totalPrice + totalShipping + tax;
-  // quantity = quantity + product.quantity;
+  
   
   return (
     <div className="cart">
